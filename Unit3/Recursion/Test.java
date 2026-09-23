dec4c85fa60452411fecb57aefe9da5b1a36bae3
harry <stackharry45@gmail.com>
2026-09-23 19:54:42 +0530

    remove duplicates from String

---
+++ Unit3/Recursion/Test.java
@@ -0,0 +1,46 @@
+package Unit3.Recursion;
+
+public class Test {
+    public static void main(String[] args) {
+        String str = "appnacollege";
+
+        removeDuplicates(str, 0, new boolean[26], new StringBuilder());
+
+        System.out.println(getString(1234567890));
+    }
+
+    public static String getString(int n) {
+        String[] nums = new String[] {
+                "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+        };
+        StringBuilder sb = new StringBuilder();
+        return getBuilder(n, sb, nums).toString();
+    }
+
+    public static StringBuilder getBuilder(int n, StringBuilder sb, String[] nums) {
+        if (n / 10 == 0) {
+            String s = nums[n % 10];
+            return sb.append(s);
+        }
+
+        getBuilder(n / 10, sb, nums);
+
+        return sb.append(" " + nums[n % 10]);
+    }
+
+    public static void removeDuplicates(String str, int idx, boolean[] isOccour, StringBuilder sb) {
+        if (idx == str.length()) {
+            System.out.println(sb);
+            return;
+        }
+
+        char curr = str.charAt(idx);
+        if (!isOccour[curr - 'a']) {
+            sb.append(curr);
+            isOccour[curr - 'a'] = true;
+        }
+
+        removeDuplicates(str, idx + 1, isOccour, sb);
+    }
+
+}
